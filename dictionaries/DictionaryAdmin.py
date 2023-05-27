131f3e97e3679342a339002b39b7667cfaead10b
danieldu <danghoangnhan.1@gmail.com>
2023-05-27 22:16:43 +0800

    disable readonly

--- dictionaries/DictionaryAdmin.py
+++ dictionaries/DictionaryAdmin.py
@@ -29,19 +29,19 @@ class DictionaryAdmin(DjangoObjectActions,
     # class Media:
     #     js = ('../static/dictionary/dictionary_admin.js',)  # Path to your JavaScript file
 
-    def get_readonly_fields(self, request, obj=None):
-        readonly_fields = super().get_readonly_fields(request, obj)
-        if obj and not obj.enable_tone_convert:
-            readonly_fields += ('convert_type',)
-        return readonly_fields
+    # def get_readonly_fields(self, request, obj=None):
+    #     readonly_fields = super().get_readonly_fields(request, obj)
+    #     if obj and not obj.enable_tone_convert:
+    #         readonly_fields += ('convert_type',)
+    #     return readonly_fields
 
-    def changeform_view(self, request, *args, **kwargs):
-        self.readonly_fields = list(self.readonly_fields)
-        usergroup = request.user.groups.filter(name__in=['author']).exists()
-        if not usergroup:
-            self.readonly_fields.append('price_upgrade')
-
-        return super(DictionaryAdmin, self).changeform_view(request, *args, **kwargs)
+    # def changeform_view(self, request, *args, **kwargs):
+    #     self.readonly_fields = list(self.readonly_fields)
+    #     usergroup = request.user.groups.filter(name__in=['author']).exists()
+    #     if not usergroup:
+    #         self.readonly_fields.append('price_upgrade')
+    #
+    #     return super(DictionaryAdmin, self).changeform_view(request, *args, **kwargs)
 
     @admin.action(description=' merge duplicated words')
     def merge_duplicated_words(self, request, queryset):
