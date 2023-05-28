451987e0c76117e3c36bb71159d2e89a930000b7
danieldu <danghoangnhan.1@gmail.com>
2023-05-28 20:38:33 +0800

    uncomment author

--- apps/settings.py
+++ apps/settings.py
@@ -41,7 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'author.middlewares.AuthorDefaultBackendMiddleware'
+    # 'author.middlewares.AuthorDefaultBackendMiddleware'
 ]
 
 
