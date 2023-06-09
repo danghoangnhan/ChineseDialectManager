a0f3baebbb54790e1f90e0e350b08ed579d675bd
danieldu <danghoangnhan.1@gmail.com>
2023-06-09 22:47:38 +0800

    change ton option

--- dictionaries/form.py
+++ dictionaries/form.py
@@ -4,19 +4,17 @@ from import_export.forms import ImportForm, ConfirmImportForm, ExportForm
 from dictionaries.models import dictionary
 from rules.models import ToneRules
 
-tone_convert_type = [("BC_AT", "BC TO AT"), ("AT_BC", "AT TO BC")]
+tone_convert_type = [("BC_AT", "Chinese"), ("AT_BC", "Taiwan")]
 enable_tone_option = [("1", "yes"), ("2", "no")]
 
 
 class VocabularyImportForm(ImportForm):
     dictionary_name = forms.ModelChoiceField(label="dictionary name", queryset=dictionary.objects.all(), required=True)
-    tone_option = forms.ChoiceField(choices=lambda: [(element['country_type'], element['country_type']) for element in
-                                                     ToneRules.objects.values('country_type').distinct()])
+    tone_option = forms.ChoiceField(choices=lambda: tone_convert_type)
 
 
 class DictionaryExportForm(ExportForm):
-    tone_option = forms.ChoiceField(choices=lambda: [(element['country_type'], element['country_type']) for element in
-                                                     ToneRules.objects.values('country_type').distinct()])
+    tone_option = forms.ChoiceField(choices=lambda: tone_convert_type)
     checkbox_field = forms.MultipleChoiceField(
         label="select dictionary",
         widget=forms.CheckboxSelectMultiple,
