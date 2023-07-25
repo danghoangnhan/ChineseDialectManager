f4a4b07471421be25d7497b1d75b9d3760f34e39
Danieldu <daniel@jasslin.com>
2023-07-25 20:39:58 +0800

    fix code

--- tone.py
+++
@@ -1,9 +0,0 @@
-class ToneConvert:
-    def __init__(self, dictionary):
-        self.dictionary = dictionary
-
-    def import_data(self, data):
-        pass
-
-    def export_data(self, data):
-        pass
